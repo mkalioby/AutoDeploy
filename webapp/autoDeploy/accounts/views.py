e44c0c8069e00f6da871d25865ad926cd6136703
Mohamed El-Kalioby <mkalioby@mkalioby.com>
2015-05-16 13:10:51 +0300

    Closes #18

---
+++ webapp/autoDeploy/accounts/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render, render_to_response,redirect
+from django.contrib.auth import authenticate, login,logout
+from django.template import RequestContext
+
+
+
+def check(request):
+    if request.method=="POST":
+        print "In Check"
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        err=""
+        print "Hi"
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                if "redirect" in request.POST:
+                    return redirect(request.POST["redirect"])
+                else:
+                    return redirect("/")
+                # Redirect to a success page.
+            else:
+                err="This user is NOT activated yet."
+        else:
+            err="The username or the password is wrong."
+        print "Error:", err
+        return render_to_response("account/login.html",{"err":err},context_instance=RequestContext(request))
+    else:
+        return render_to_response("account/login.html",context_instance=RequestContext(request))
+
+def signOut(request):
+    logout(request)
+    return render_to_response("account/logout.html",context_instance=RequestContext(request))
--- webapp/autoDeploy/autoDeploy/urls.py
+++ webapp/autoDeploy/autoDeploy/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+import accounts.urls
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/', include(accounts.urls)),
     url(r'^$','autodeploy.views.projects'),
     url(r'add_project','autodeploy.views.add_project'),
     url(r'add_server','autodeploy.views.add_server'),
