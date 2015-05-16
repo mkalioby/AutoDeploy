d4bf919c98a6edb9744f550b8674715c08a3470e
Mohamed El-Kalioby <mkalioby@mkalioby.com>
2015-05-16 10:48:49 +0300

    Closes #22

--- webapp/autoDeploy/static/js/functions.js
+++ webapp/autoDeploy/static/js/functions.js
@@ -20,3 +20,16 @@ function checkServers(servers)
     html+='</table>'
     $("#content").html(html)
     }
+
+
+function clone(data) {
+    html=""
+    console.log(data)
+    if (data == "Done")
+        html = "<div class='alert alert-success'>Cloning is succesful</div>"
+    else
+        html = "<div class='alert alert-danger'>" + data + "</div>"
+
+
+    $("#content").html(html)
+}
