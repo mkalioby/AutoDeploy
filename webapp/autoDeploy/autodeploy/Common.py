1ef405748ba4045458eca3a8561b33fa6a93cd00
Mohamed ElKalioby <melkalioby@kfshrc.edu.sa>
2017-09-18 15:53:21 +0300

    Moved to Django Emailer

--- webapp/autoDeploy/autodeploy/Common.py
+++ webapp/autoDeploy/autodeploy/Common.py
@@ -1,39 +1,17 @@
 __author__ = 'mohamed'
 
 def send(to,subject,body,fromUser=None,cc="",bcc="",):
-
     from django.conf import settings
-    import smtplib
-    from email.mime.multipart import MIMEMultipart
-    from email.mime.text import MIMEText
-    From=fromUser
-    if fromUser==None:
-        From="%s<%s>"%(settings.SMTP["FROM"],settings.SMTP["USERNAME"])
-    try:
-            msg = MIMEMultipart('alternative')
-            msg['Subject'] = subject
-            msg['From'] = From
-            #msg['To'] = to one email
-            msg['Cc']= cc
-            if type(to)==type([]):
-                msg['To'] = ', '.join( to) #More than one email
-            else:
-                msg['To'] = to
-            html = body
-            part2 = MIMEText(html, 'html')
-            msg.attach(part2)
-            server = smtplib.SMTP(settings.SMTP["HOST"],settings.SMTP["PORT"])
-            server.ehlo()
-            server.starttls()
-            server.login(settings.SMTP["USERNAME"], settings.SMTP["PASSWORD"])
-            for t in to.split(";"):
-                server.sendmail(From, t, msg.as_string())
-            if cc!="":
-                server.sendmail(From, cc, msg.as_string())
-            if bcc!="":
-                server.sendmail(From, bcc, msg.as_string())
-            server.quit()
-            return True
-
-    except Exception as exp:
-        return False
+    from django.core.mail import EmailMessage
+    From = "%s<%s>" % (fromUser, settings.EMAIL_HOST_USER)
+    if fromUser == None:
+        From = "%s<%s>" % (settings.EMAIL_FROM, settings.EMAIL_HOST_USER)
+    if type(to) != type([]):
+        to = [to]
+    email = EmailMessage(
+        subject,
+        body,
+        From,
+        to, cc=cc, bcc=bcc)
+    email.content_subtype = "html"
+    return email.send(True)
