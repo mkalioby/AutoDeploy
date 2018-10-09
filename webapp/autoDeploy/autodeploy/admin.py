3ef02f1f2151cfe38f2978fa6f7a16de6cf76e79
Mohamed ElKalioby <melkalioby@kfshrc.edu.sa>
2018-10-09 17:41:05 +0300

    Handling issues during Admin Registeration

--- webapp/autoDeploy/autodeploy/admin.py
+++ webapp/autoDeploy/autodeploy/admin.py
@@ -4,7 +4,10 @@ from django.apps import apps
 # Register your models here.
 def autoregister(*app_list):
     for model in apps.get_models():
+        try:
                 admin.site.register(model)
+        except Exception:
+            pass
 
 autoregister('autodeploy', 'admin')
 
