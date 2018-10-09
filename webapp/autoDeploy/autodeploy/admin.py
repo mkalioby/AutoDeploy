ddb83b275e1b921afc508e8c4868c07b6bddd5ba
Mohamed ElKalioby <melkalioby@kfshrc.edu.sa>
2018-10-09 17:36:43 +0300

    Adding Admin Static content, add all models to admin

--- webapp/autoDeploy/autodeploy/admin.py
+++ webapp/autoDeploy/autodeploy/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from django.apps import apps
 
 # Register your models here.
+def autoregister(*app_list):
+    for model in apps.get_models():
+                admin.site.register(model)
+
+autoregister('autodeploy', 'admin')
+
